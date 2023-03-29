2286105fbe037c6287237ac555b0528cba425fe2
Samuel Burbano <me@iosamuel.dev>
2023-03-29 15:14:31 -0500

    feat: todo estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-import { createEffect, createMemo, createSignal } from "solid-js";
-
 function App() {
-  const [count, setCount] = createSignal(0);
-
-  // Signals Derivadas
-  const doubleCount = () => count() * 2;
-
-  // Signals Memoizadas
-  const isDivisibleByThree = createMemo(() => count() % 3 === 0);
-
-  createEffect(() => {
-    console.log("count changed", count());
-  });
-
-  createEffect(() => {
-    console.log("isDivisibleByThree changed", isDivisibleByThree());
-  });
-
   return (
-    <>
-      <h1>Count: {count()}</h1>
-      <h2>Double: {doubleCount()}</h2>
-      <h2>Is divisible by three: {isDivisibleByThree() ? "Yes" : "No"}</h2>
-      <button onClick={() => setCount(count() + 1)}>{count()}</button>
-    </>
+    <div class="w-full h-full min-h-screen bg-slate-500">Hello, Solid!</div>
   );
 }
 
