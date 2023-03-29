96bbe8dfde25e0a2601e6a24b5ef263de77a5016
Samuel Burbano <me@iosamuel.dev>
2023-03-29 14:02:41 -0500

    feat: primitivas reactivas - effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 
 function App() {
   const [count, setCount] = createSignal(0);
+
+  createEffect(() => {
+    console.log("count changed", count());
+  });
+
   return (
     <>
       <h1>Count: {count()}</h1>
