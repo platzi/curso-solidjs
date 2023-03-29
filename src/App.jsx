a66a4a76912c81221679e12d5b8722a78334e650
Samuel Burbano <me@iosamuel.dev>
2023-03-29 16:00:02 -0500

    feat: todo dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { createEffect, createSignal } from "solid-js";
+
 function App() {
+  const [darkMode, setDarkMode] = createSignal(false);
+
+  createEffect(() => {
+    document.body.classList.toggle("dark", darkMode());
+  });
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode());
+  }
+
   return (
-    <div class="w-full h-full min-h-screen flex items-center justify-center bg-slate-500">
-      <button class="text-2xl fixed top-0 right-0">🌚</button>
+    <div class="w-full h-full min-h-screen flex items-center justify-center dark:bg-gray-600 dark:text-white">
+      <button
+        class="text-2xl fixed top-0 right-0"
+        onClick={() => toggleDarkMode()}
+      >
+        🌚
+      </button>
 
       <div>
         <h1 class="text-2xl text-center">Solid Todo App</h1>
-        <input type="text" />
+        <input class="border dark:text-black" type="text" />
         <button class="px-2 border">Add</button>
         <ul>
           <li>
