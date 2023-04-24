cc800f8f66245e586eb06bcde3223028bb1f6646
Samuel Burbano <me@iosamuel.dev>
2023-04-24 15:14:47 -0500

    feat: primitivas reactivas - effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { createEffect, createSignal } from "solid-js";
 
 function App() {
   const [count, setCount] = createSignal(0);
+  const [count2, setCount2] = createSignal(0);
 
   createEffect(() => {
-    console.log("count changed", count());
+    console.log("Count changed", count());
+    console.log("Count changed", count());
+    console.log("Count changed 2", count2());
+  });
+
+  createEffect(() => {
+    sessionStorage.setItem("count", count());
   });
 
   return (
     <>
       <h1>Count: {count()}</h1>
       <button onClick={() => setCount(count() + 1)}>{count()}</button>
+      <button onClick={() => setCount2(count2() + 1)}>{count2()}</button>
     </>
   );
 }
